e6ed4eb24961c5cd322d2c9a8d96255504d99214
animal <sidafra@gmail.com>
2022-01-24 01:03:28 +0900

    백버튼 종료 로직 추가

--- lib/src/presentation/view/main/main_view.dart
+++ lib/src/presentation/view/main/main_view.dart
@@ -5,39 +5,58 @@ import 'package:artitecture/src/presentation/view/main/tab_one.dart';
 import 'package:artitecture/src/presentation/view/main/tab_three.dart';
 import 'package:artitecture/src/presentation/view/main/tab_two.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 import 'package:get/get.dart';
 
-class MainPage extends StatelessWidget {
+class MainPage extends HookWidget {
   const MainPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     final MainController _mainController = Get.put(injector());
     final List<Widget> _children = [const TabOne(), const TabTwo(), const TabThree()];
+    final _prevPressedTime = useState(DateTime.now());
 
-    return Obx(() => Scaffold(
-      body: Center(
-        child: _children[_mainController.tabIndex.value]
-      ),
-      bottomNavigationBar: BottomNavigationBar(
-        items: const <BottomNavigationBarItem>[
-          BottomNavigationBarItem(
-            icon: Icon(Icons.call),
-            label: 'Calls',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.camera),
-            label: 'Camera',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.chat),
-            label: 'Chats',
-          ),
-        ],
-        onTap: (int index) => _mainController.selectTab(index),
-        currentIndex: _mainController.tabIndex.value,
-        backgroundColor: primaryColor,
-      ),
-    ));
+    return WillPopScope(
+      onWillPop: () async {
+        final timeDiff = DateTime.now().difference(_prevPressedTime.value);
+        final isFirstClick = timeDiff >= const Duration(seconds: 2);
+        _prevPressedTime.value = DateTime.now();
+        if (isFirstClick) {
+          await Fluttertoast.showToast(
+              msg: "종료하시려면 한번 더 눌러주세요",
+              toastLength: Toast.LENGTH_SHORT
+          );
+          return false;
+        } else {
+          return true;
+        }
+      },
+      child: Obx(() => Scaffold(
+        body: Center(
+          child: _children[_mainController.tabIndex.value]
+        ),
+        bottomNavigationBar: BottomNavigationBar(
+          items: const <BottomNavigationBarItem>[
+            BottomNavigationBarItem(
+              icon: Icon(Icons.call),
+              label: 'Calls',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.camera),
+              label: 'Camera',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.chat),
+              label: 'Chats',
+            ),
+          ],
+          onTap: (int index) => _mainController.selectTab(index),
+          currentIndex: _mainController.tabIndex.value,
+          backgroundColor: primaryColor,
+        ),
+      )),
+    );
   }
 }
