02d08ddf3dc83d8b62436d119e61916e162795d1
animal <sidafra@gmail.com>
2022-02-07 01:02:20 +0900

    갤러리 추가

--- lib/src/presentation/view/main/tab_two.dart
+++ lib/src/presentation/view/main/tab_two.dart
@@ -1,10 +1,18 @@
+import 'package:artitecture/src/presentation/route.dart';
+import 'package:artitecture/src/presentation/view/photo/gallery.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 class TabTwo extends StatelessWidget {
   const TabTwo({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Text('TabTwo');
+    return ElevatedButton(
+      onPressed: () {
+        Get.toNamed(galleryRoute, arguments: GalleryParams(images: ['https://picsum.photos/200/300', 'https://picsum.photos/200', 'https://picsum.photos/id/237/200/300'], index: 0));
+      },
+      child: const Text('gallery'),
+    );
   }
 }
--- lib/src/presentation/view/photo/gallery.dart
+++ lib/src/presentation/view/photo/gallery.dart
@@ -1,11 +1,72 @@
+import 'package:dots_indicator/dots_indicator.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:get/get.dart';
+import 'package:photo_view/photo_view.dart';
+import 'package:photo_view/photo_view_gallery.dart';
 
-class GalleryPage extends HookWidget {
+class GalleryPage extends StatefulWidget {
   const GalleryPage({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
-    return Container();
+  State<StatefulWidget> createState() {
+    return _GalleryPageState();
   }
+}
+
+class _GalleryPageState extends State<GalleryPage> {
+  late List<String> _images;
+  late int _index;
+
+  @override
+  void initState() {
+    final _params = Get.arguments as GalleryParams;
+    _images = _params.images;
+    _index = _params.index;
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Stack(
+        children: [
+          PhotoViewGallery.builder(
+            scrollPhysics: const BouncingScrollPhysics(),
+            builder: (context, index) => _buildItem(_images[index], index),
+            itemCount: _images.length,
+            onPageChanged: (index) => setState(() {
+              _index = index;
+            }),
+            scrollDirection: Axis.horizontal,
+          ),
+          Align(
+            alignment: Alignment.bottomCenter,
+            child: DotsIndicator(
+              dotsCount: _images.length,
+              position: _index.toDouble(),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+
+  PhotoViewGalleryPageOptions _buildItem(String path, int index) {
+    return PhotoViewGalleryPageOptions(
+      imageProvider: NetworkImage(path),
+      initialScale: PhotoViewComputedScale.contained,
+      minScale: PhotoViewComputedScale.contained * 1,
+      maxScale: PhotoViewComputedScale.contained * 5,
+      heroAttributes: PhotoViewHeroAttributes(tag: path),
+    );
+  }
+}
+
+class GalleryParams {
+  List<String> images;
+  int index;
+
+  GalleryParams({
+    required this.images, required this.index
+  });
 }
