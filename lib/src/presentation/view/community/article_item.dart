de58729b526042e1925f798017d64dc5b69c53d2
animal <sidafra@gmail.com>
2022-02-09 23:24:30 +0900

    포매터 적용

--- lib/src/presentation/view/community/article_item.dart
+++ lib/src/presentation/view/community/article_item.dart
@@ -1,6 +1,5 @@
 import 'package:artitecture/src/core/utils/text_utils.dart';
 import 'package:artitecture/src/domain/entity/response/article.dart';
-import 'package:artitecture/src/domain/entity/response/image.dart' as model;
 import 'package:artitecture/src/presentation/route.dart';
 import 'package:artitecture/src/presentation/view/photo/gallery.dart';
 import 'package:cached_network_image/cached_network_image.dart';
@@ -21,49 +20,47 @@ class ArticleItemView extends StatelessWidget {
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              Text(_article.title,
-                  style: const TextStyle(
-                      fontSize: 20,
-                      fontWeight: FontWeight.bold
-                  )
+              Text(
+                _article.title,
+                style: const TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                ),
               ),
-              const SizedBox(
-                height: 10,
-              ),
-              Text(_article.contents,
-                  style: const TextStyle(
-                      fontSize: 16
-                  )
+              const SizedBox(height: 10),
+              Text(
+                _article.contents,
+                style: const TextStyle(fontSize: 16),
               ),
               _article.images.isNotEmpty
-                  ? Column(children: [
-                const SizedBox(height: 14),
-                getImageViews(_article.images.map((e) => e.imageUrl).toList())
-              ])
-                  : Container()
-              ,
-              const SizedBox(
-                height: 15,
-              ),
+                  ? Column(
+                      children: [
+                        const SizedBox(height: 14),
+                        getImageViews(_article.images.map((e) => e.imageUrl).toList()),
+                      ],
+                    )
+                  : Container(),
+              const SizedBox(height: 15),
               Row(
                 children: [
                   Text(_article.author.nickname),
                   const Spacer(),
-                  Text(TextUtils.getCreatedBefore(_article.createdDate))
+                  Text(TextUtils.getCreatedBefore(_article.createdDate)),
                 ],
               ),
             ],
           ),
         ),
-        const SizedBox(
-          height: 14,
-        ),
+        const SizedBox(height: 14),
         const Divider(
           height: 1,
           color: Colors.grey,
         ),
         Container(
-          padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 12),
+          padding: const EdgeInsets.symmetric(
+            horizontal: 20,
+            vertical: 12,
+          ),
           child: Row(
             children: [
               InkWell(
@@ -71,7 +68,7 @@ class ArticleItemView extends StatelessWidget {
                   children: const [
                     Icon(Icons.favorite_border),
                     SizedBox(width: 8),
-                    Text('좋아요')
+                    Text('좋아요'),
                   ],
                 ),
               ),
@@ -81,12 +78,10 @@ class ArticleItemView extends StatelessWidget {
                   children: [
                     const Icon(Icons.chat_bubble_outline),
                     const SizedBox(width: 8),
-                    _article.commentCount == 0
-                        ? const Text('댓글쓰기')
-                        : Text('답변 ${_article.commentCount}')
+                    _article.commentCount == 0 ? const Text('댓글쓰기') : Text('답변 ${_article.commentCount}'),
                   ],
                 ),
-              )
+              ),
             ],
           ),
         )
@@ -98,69 +93,71 @@ class ArticleItemView extends StatelessWidget {
     return ClipRRect(
       borderRadius: BorderRadius.circular(8.0),
       child: AspectRatio(
-          aspectRatio: 16 / 9,
-          child: images.length == 1
-              ? getImageView(images, 0)
-              : images.length == 2
-              ? Row(children: [
-            Flexible(
-                flex: 1,
-                fit: FlexFit.tight,
-                child: getImageView(images, 0)
-            ),
-            const SizedBox(width: 4),
-            Flexible(
-                flex: 1,
-                fit: FlexFit.tight,
-                child: getImageView(images, 1)
-            )
-          ],
-          ) : Row(
-            children: [
-              Flexible(
-                  flex: 2,
-                  fit: FlexFit.tight,
-                  child: getImageView(images, 0)
-              ),
-              const SizedBox(width: 4),
-              Flexible(
-                  flex: 1,
-                  fit: FlexFit.tight,
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.stretch,
+        aspectRatio: 16 / 9,
+        child: images.length == 1
+            ? getImageView(images, 0)
+            : images.length == 2
+                ? Row(
                     children: [
-                      Expanded(
-                          child: getImageView(images, 1)
+                      Flexible(
+                        flex: 1,
+                        fit: FlexFit.tight,
+                        child: getImageView(images, 0),
                       ),
-                      const SizedBox(height: 4),
-                      Expanded(
-                          child: images.length > 3 ?
-                          Stack(
-                              children: [
-                                Positioned.fill(
-                                    child: getImageView(images, 2)
-                                ),
-                                IgnorePointer(
-                                  child: Container(
-                                      color: Colors.black26
-                                  ),
-                                ),
-                                IgnorePointer(
-                                  child: Positioned.fill(
-                                      child: Align(
-                                          alignment: Alignment.center,
-                                          child: Text('+ ${images.length - 3}', style: const TextStyle(color: Colors.white, fontWeight: FontWeight.bold, fontSize: 20))
-                                      )
-                                  ),
-                                )
-                              ]) :
-                          getImageView(images, 2)
-                      )
+                      const SizedBox(width: 4),
+                      Flexible(
+                        flex: 1,
+                        fit: FlexFit.tight,
+                        child: getImageView(images, 1),
+                      ),
                     ],
                   )
-              ),
-            ],
-          )
+                : Row(
+                    children: [
+                      Flexible(
+                        flex: 2,
+                        fit: FlexFit.tight,
+                        child: getImageView(images, 0),
+                      ),
+                      const SizedBox(width: 4),
+                      Flexible(
+                        flex: 1,
+                        fit: FlexFit.tight,
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.stretch,
+                          children: [
+                            Expanded(child: getImageView(images, 1)),
+                            const SizedBox(height: 4),
+                            Expanded(
+                              child: images.length > 3
+                                  ? Stack(
+                                      children: [
+                                        Positioned.fill(child: getImageView(images, 2)),
+                                        IgnorePointer(child: Container(color: Colors.black26)),
+                                        IgnorePointer(
+                                          child: Positioned.fill(
+                                            child: Align(
+                                              alignment: Alignment.center,
+                                              child: Text(
+                                                '+ ${images.length - 3}',
+                                                style: const TextStyle(
+                                                  color: Colors.white,
+                                                  fontWeight: FontWeight.bold,
+                                                  fontSize: 20,
+                                                ),
+                                              ),
+                                            ),
+                                          ),
+                                        ),
+                                      ],
+                                    )
+                                  : getImageView(images, 2),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
       ),
     );
   }
@@ -168,12 +165,18 @@ class ArticleItemView extends StatelessWidget {
   Widget getImageView(List<String> images, int index) {
     return InkWell(
       child: CachedNetworkImage(
-          placeholder: (context, url) => const Icon(Icons.photo),
-          errorWidget: (context, url, error) => const Icon(Icons.photo),
-          fit: BoxFit.cover,
-          imageUrl: images[index]
+        placeholder: (context, url) => const Icon(Icons.photo),
+        errorWidget: (context, url, error) => const Icon(Icons.photo),
+        fit: BoxFit.cover,
+        imageUrl: images[index],
       ),
-      onTap: () => Get.toNamed(galleryRoute, arguments: GalleryParams(images: images, index: index)),
+      onTap: () => Get.toNamed(
+        galleryRoute,
+        arguments: GalleryParams(
+          images: images,
+          index: index,
+        ),
+      ),
     );
   }
 }
